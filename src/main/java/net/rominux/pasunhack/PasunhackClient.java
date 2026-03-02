aa1a2e6920f2b9205c2873064696af0b4fe1c7a0
rominux <romain.59.@gmail.com>
2026-03-02 19:19:22 +0100

    commit

--- src/main/java/net/rominux/pasunhack/PasunhackClient.java
+++ src/main/java/net/rominux/pasunhack/PasunhackClient.java
@@ -4,7 +4,6 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
-import net.minecraft.client.util.InputUtil;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 
@@ -14,10 +13,9 @@ public class PasunhackClient implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
-        // Enregistrement de la touche F6
+        // Enregistrement de la touche F6 avec le constructeur à 3 arguments (plus stable)
         toggleKey = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 "key.pasunhack.toggle_autominer",
-                InputUtil.Type.KEYSYM,
                 GLFW.GLFW_KEY_F6,
                 "category.pasunhack.main"));
 
